2a8e9518e6cfd240e71b5c2753824952fd976289
Alex Anthis <anthiswebdev@gmail.com>
2025-03-27 10:24:10 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -73,5 +73,5 @@ $(function() {
   sr.reveal('.skills');
   sr.reveal('.experience', { viewFactor: 0.1 });
   sr.reveal('.featured-projects', { viewFactor: 0.1 });
-  sr.reveal('.other-projects', { viewFactor: 0.2 });
+  sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
